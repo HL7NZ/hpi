b50f933b53da87afa7413ac26ca48074d72c1868
pryan <pryan@EC2AMAZ-TALBF09.health.local>
2022-06-29 00:08:11 +0000

    adding openapi spec

--- build-ig.sh
+++ build-ig.sh
@@ -8,3 +8,4 @@ echo running sushi ...
 
 echo running ig publisher
 java -jar C:/apps/FHIR_IG_Publisher/publisher.jar -ig . -proxy WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128 -no-sushi -tx  http://tx.fhir.org 
+cp ./openapi/HpiFhirOpenApi.yaml ./output/HpiFhirOpenApi.yaml
