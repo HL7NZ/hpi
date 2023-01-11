1f50949e6d898788f5ae2bbc75a1333869df0e16
pryan <pryan@EC2AMAZ-TALBF09.health.local>
2023-01-11 03:53:54 +0000

    fixed nzbase path

--- build-ig-cicd.sh
+++ build-ig-cicd.sh
@@ -15,7 +15,7 @@ echo nzbase version =$nzbase_version
 #cp nzbase into user's .fhir cache 
 sudo mkdir temp
 cd temp
-wget -e use_proxy=yes -e https_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"\package.tgz"
+wget -e use_proxy=yes -e https_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"/package.tgz"
 tar zxvf package.tgz
 ##fix the package url:
 jq --arg url $nzbase_url '.url |= $url' ./package/package.json > temp.json
