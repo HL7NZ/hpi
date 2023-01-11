a6b556df40e648c13c690335e7a78d76b68aac81
pryan <pryan@EC2AMAZ-TALBF09.health.local>
2023-01-11 03:31:21 +0000

    changed nzbaseurl

--- build-ig-cicd.sh
+++ build-ig-cicd.sh
@@ -12,20 +12,14 @@ nzbase_version=$(yq '.dependencies."fhir.org.nz.ig.base".version' ./sushi-config
 echo nzbase url =$nzbase_url
 echo nzbase version =$nzbase_version
 
-
-wget -e use_proxy=yes -e http_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"/full-ig.zip"
-unzip ./full-ig.zip site/package.tgz
-
-tar zxvf ./site/package.tgz
-#fix the package url:
+#cp nzbase into user's .fhir cache 
+sudo mkdir temp
+cd temp
+wget -e use_proxy=yes -e https_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"\package.tgz"
+tar zxvf package.tgz
+##fix the package url:
 jq --arg url $nzbase_url '.url |= $url' ./package/package.json > temp.json
 mv temp.json ./package/package.json 
-
-
-#cp nz packages  into user's .fhir cache 
-sudo mkdir -p  ~/.fhir/packages/fhir.org.nz.ig.base#$nzbase_version/package
-sudo  cp -r ./package ~/.fhir/packages/fhir.org.nz.ig.base#$nzbase_version
-
 
 #cp hl7 packages into user's .fhir cache 
 aws s3 cp s3://nz-govt-moh-hip-build/codebuild-common/fhir/hl7.fhir.r4.core#4.0.1/package.zip ./hl7-package.zip
