07d59b3e83d1fef36b848feff27fd12075860053
pryan <pat.ryan@health.govt.nz>
2023-06-20 21:06:28 +0000

    fix nz base (cant get zip anymore)

--- build-ig-cicd.sh
+++ build-ig-cicd.sh
@@ -13,18 +13,19 @@ echo nzbase url =$nzbase_url
 echo nzbase version =$nzbase_version
 
 
-wget -e use_proxy=yes -e http_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"/full-ig.zip"
-unzip ./full-ig.zip site/package.tgz
-
-tar zxvf ./site/package.tgz
-#fix the package url:
+#cp nzbase into user's .fhir cache 
+sudo mkdir temp
+cd temp
+wget -e use_proxy=yes -e https_proxy=WebProxy-80fef376c00ea74f.elb.ap-southeast-2.amazonaws.com:3128  $nzbase_url"package.tgz"
+tar zxvf package.tgz
+##fix the package url:
 jq --arg url $nzbase_url '.url |= $url' ./package/package.json > temp.json
 mv temp.json ./package/package.json 
 
-
-#cp nz packages  into user's .fhir cache 
+##cp nz packages  into user's .fhir cache 
 sudo mkdir -p  ~/.fhir/packages/fhir.org.nz.ig.base#$nzbase_version/package
 sudo  cp -r ./package ~/.fhir/packages/fhir.org.nz.ig.base#$nzbase_version
+cd ..
 
 
 #cp hl7 packages into user's .fhir cache 
